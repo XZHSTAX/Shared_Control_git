b73a1a5bc9113573416a5f77740b2d9021a5cfad
XZH_Linux <XZHSTAX@foxmail.com>
2022-04-16 22:07:48 +0800

    some test

--- Shared_Control_continue.py
+++ Shared_Control_continue.py
@@ -19,7 +19,7 @@ ENV_ID = 'LunarLanderContinuous_SC-v2'
 
 #Done 添加当用户不操作，就让机器接管；注意，是用户不操作，即不按键
 key_flag = [0,0,0,0]   # ? 用于储存上下左右是否被按下，按下为1，抬起为0
-inverse_control = 0    # ? 0 inverse 1 normal   i use 0
+inverse_control = 1    # ? 0 inverse 1 normal   i use 0
 
 def key_press(key, mod):
   """
@@ -97,6 +97,49 @@ def human_pilot_policy(obs):
   global human_agent_action
   return disc_to_cont(encode_human_action(human_agent_action)),encode_human_action(human_agent_action)
 
+def how_sim(a1,a2):
+    X = np.array([[2,1,1,1,0,0],
+                  [1,2,1,0,0,0],
+                  [1,1,2,0,0,1],
+                  [1,0,0,2,1,1],
+                  [0,0,0,1,2,1],
+                  [0,0,1,1,1,2]])
+    return X[a1,a2]    
+human_action_taken_num = 0
+def human_copilot_action_arbitration(copilot_Q,pilot_action,arbitration_way =1):
+    """
+    人机交互仲裁，arbitration_way =1最优动作替换,arbitration_way =2 相似动作替换
+    输入： copilot_Q：Copilot对当前动作的评估值；pilot_action飞行员输入的动作
+    输出： 仲裁后，认为最优的动作
+    """
+    global human_action_taken_num
+    copilot_Qmax = np.max(copilot_Q)                            # ? copilot 认为最佳动作的Q值
+    copilot_action = np.argmax(copilot_Q)                        # ? copilot 认为最佳动作
+
+    if arbitration_way==1:
+        if copilot_Q[int(pilot_action)] >= copilot_Qmax*0.975:
+            human_action_taken_num +=1
+            return pilot_action
+        else:
+            return copilot_action
+    elif arbitration_way==2:
+
+        copilot_Qmin = np.min(copilot_Q)                            # ? copilot 认为最差动作的Q值
+        action_mask = ((copilot_Q-copilot_Qmin*np.ones(copilot_Q.shape)) >= 0.9*(copilot_Qmax-copilot_Qmin))
+        action_sim = how_sim(np.array([0,1,2,3,4,5]),pilot_action)
+
+        action_chooseable =  action_mask*action_sim
+        if not any(action_chooseable):                             # ? 如果全是0
+            return copilot_action                                  # ? 返回copilot的动作
+        elif np.sum(action_chooseable==1)<=1 or np.sum(action_chooseable==2)==1:  # ? 如果就1个1或有2
+            human_action_taken_num +=1
+            return np.argmax(action_chooseable)                                   # ? 返回最佳动作
+        else:
+            a = np.nonzero(action_chooseable==1)
+            a = np.array(a)[0]
+            human_action_taken_num +=1
+            return a[np.argmax(np.random.rand(a.shape[0]))]                       # ? 从相同的最佳中随机抽取
+
 def create_model(input_state_shape):
     input_layer = tl.layers.Input(input_state_shape)
     layer_1 = tl.layers.Dense(n_units=256, act=tf.nn.relu)(input_layer)
@@ -112,7 +155,7 @@ model.eval()
 
 # ! 可调参数--------------------------------------
 test_episodes = 10  # ? 测试次数
-pilot_name = '测试-ez'  # ? 驾驶员名称
+pilot_name = 'gsf-test'  # ? 驾驶员名称
 alpha = 0.96        # ? 相似系数
 load_model_path = 'model/A3_plus2.5' # ? 加载模型位置
 if os.path.exists(load_model_path):
@@ -138,7 +181,7 @@ state = state[:8]
 done_num = 0
 success_times = 0
 crash_times = 0
-human_action_taken_num = 0
+
 total_time = 1
 total_reward = 0
 
@@ -156,15 +199,21 @@ while 1:
     bot_action = np.argmax(bot_action_Q)
 
 
+    # if not any(key_flag):
+    #     # ? 如果没有按键按下，key_flag全0，any(key_flag)返回false
+    #     # ? 有按键按下，key_flag非全0      any(key_flag)返回True
+    #     action = disc_to_cont(bot_action)
+    # if bot_action_Q[encode_action] <= bot_action_Q[bot_action]*alpha:
+    #     action = disc_to_cont(bot_action)
+    # else:
+    #     action = human_action
+    #     human_action_taken_num+=1   
     if not any(key_flag):
         # ? 如果没有按键按下，key_flag全0，any(key_flag)返回false
         # ? 有按键按下，key_flag非全0      any(key_flag)返回True
         action = disc_to_cont(bot_action)
-    if bot_action_Q[encode_action] <= bot_action_Q[bot_action]*alpha:
-        action = disc_to_cont(bot_action)
     else:
-        action = human_action
-        human_action_taken_num+=1    
+        action = disc_to_cont(human_copilot_action_arbitration(bot_action_Q.numpy(),encode_action))     
     
     next_state, reward, done, info = env.step(action)
     next_state = next_state[:8]
